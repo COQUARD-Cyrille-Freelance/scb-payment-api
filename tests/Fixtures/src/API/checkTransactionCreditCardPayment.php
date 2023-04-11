923c44b857c4d33d97eb3a3b46b527807ac2f272
cyrille <coquardcyr@gmail.com>
2023-04-11 15:41:01 +0200

    Added tests for checkTransactionCreditCardPayment.php

--- tests/Fixtures/src/API/checkTransactionCreditCardPayment.php
+++ tests/Fixtures/src/API/checkTransactionCreditCardPayment.php
@@ -1,4 +1,135 @@
 <?php
 return [
-
+    'NotConfiguredShouldRaiseAnError' => [
+        'config' => [
+            'properties' => [
+                'token' => '',
+                'baseURL' => 'http://api.partners.scb/partners',
+                'language' => 'EN',
+                'appId' => 'application_id',
+                'merchant' => 'merchant_id',
+                'terminal' => 'terminal_id',
+                'biller' => 'biller_id',
+                'prefix' => 'prefix',
+            ],
+            'is_error' => false,
+            'is_configured' => false,
+            'transaction_id' => 'transaction_id',
+            'response_content' => json_encode([
+                'status' => [
+                    'code' => 1000,
+                ],
+                'data' => [
+                    (object) [
+                        'test' => 'test'
+                    ]
+                ]
+            ]),
+        ],
+        'expected' => [
+            'body' => json_encode([]),
+            'method' => 'GET',
+            'uri' => 'http://api.partners.scb/partners/v1/payment/qrcode/creditcard/transaction_id',
+            'data' => [
+                (object) [
+                    'test' => 'test'
+                ]
+            ],
+            'headers' => [
+                'requestUId' => Mockery::any(),
+                'accept-language' => 'EN',
+                'Content-Type' => 'application/json',
+                'authorization' => 'Bearer token',
+                'resourceOwnerId' => 'application_id',
+            ]
+        ]
+    ],
+    'ValidShouldReturnData' => [
+        'config' => [
+            'properties' => [
+                'token' => 'token',
+                'baseURL' => 'http://api.partners.scb/partners',
+                'language' => 'EN',
+                'appId' => 'application_id',
+                'merchant' => 'merchant_id',
+                'terminal' => 'terminal_id',
+                'biller' => 'biller_id',
+                'prefix' => 'prefix',
+            ],
+            'is_error' => false,
+            'is_configured' => true,
+            'transaction_id' => 'transaction_id',
+            'response_content' => json_encode([
+                'status' => [
+                    'code' => 1000,
+                ],
+                'data' => [
+                    (object) [
+                        'test' => 'test'
+                    ]
+                ]
+            ]),
+        ],
+        'expected' => [
+            'body' => json_encode([]),
+            'method' => 'GET',
+            'uri' => 'http://api.partners.scb/partners/v1/payment/qrcode/creditcard/transaction_id',
+            'data' => [
+                (object) [
+                    'test' => 'test'
+                ]
+            ],
+            'headers' => [
+                'requestUId' => Mockery::any(),
+                'accept-language' => 'EN',
+                'Content-Type' => 'application/json',
+                'authorization' => 'Bearer token',
+                'resourceOwnerId' => 'application_id',
+            ]
+        ]
+    ],
+    'FailRequestShouldRaiseError' => [
+        'config' => [
+            'properties' => [
+                'token' => 'token',
+                'baseURL' => 'http://api.partners.scb/partners',
+                'language' => 'EN',
+                'appId' => 'application_id',
+                'merchant' => 'merchant_id',
+                'terminal' => 'terminal_id',
+                'biller' => 'biller_id',
+                'prefix' => 'prefix',
+            ],
+            'is_error' => true,
+            'is_configured' => true,
+            'transaction_id' => 'transaction_id',
+            'response_content' => json_encode([
+                'status' => [
+                    'code' => 1000,
+                ],
+                'data' => [
+                    (object) [
+                        'test' => 'test'
+                    ]
+                ]
+            ]),
+        ],
+        'expected' => [
+            'body' => json_encode([]),
+            'method' => 'GET',
+            'uri' => 'http://api.partners.scb/partners/v1/payment/qrcode/creditcard/transaction_id',
+            'data' => [
+                (object) [
+                    'test' => 'test'
+                ]
+            ],
+            'headers' => [
+                'requestUId' => Mockery::any(),
+                'accept-language' => 'EN',
+                'Content-Type' => 'application/json',
+                'authorization' => 'Bearer token',
+                'resourceOwnerId' => 'application_id',
+            ]
+        ]
+    ]
 ];
